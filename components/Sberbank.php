df503bf2c52e0096b201d06680e28ba77bc07646
dven84 <dven84@gmail.com>
2023-01-26 14:49:14 +0500

    Отключение проверки SSL
    В своих модулях сбербанк решает проблему аналогично
    https://securepayments.sberbank.ru/wiki/doku.php/certificates:add:cms:opencart

--- components/Sberbank.php
+++ components/Sberbank.php
@@ -122,6 +122,8 @@ class Sberbank extends Component
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
         $out = curl_exec($curl);
         curl_close($curl);
